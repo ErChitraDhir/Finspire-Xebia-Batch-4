b9b6a388fb6127d4e054d7b7dfdb30717a8af0b4
Mayank Aggarwal <mayankdatabase04@gmail.com>
2024-06-19 12:52:16 +0530

    Updated Customer model by creating nesting in name and address.

--- Backend/models/Customer.js
+++ Backend/models/Customer.js
@@ -3,13 +3,37 @@ const { isEmail } = require("validator");
 
 const customerSchema = new mongoose.Schema(
   {
-    firstName: {
-      type: String,
-      required: [true, "Please Enter First Name"],
+    name: {
+      first: {
+        type: String,
+        required: [true, "Please Enter First Name"],
+      },
+      middle: {
+        type: String,
+        required: false, // Middle name is optional
+      },
+      last: {
+        type: String,
+        required: [true, "Please Enter Last Name"],
+      },
     },
-    lastName: {
-      type: String,
-      required: [true, "Please Enter Last Name"],
+    dateOfBirth: {
+      day: {
+        type: Number,
+        required: [true, "Please Enter Day of Birth"],
+        min: 1,
+        max: 31,
+      },
+      month: {
+        type: Number,
+        required: [true, "Please Enter Month of Birth"],
+        min: 1,
+        max: 12,
+      },
+      year: {
+        type: Number,
+        required: [true, "Please Enter Year of Birth"],
+      },
     },
     email: {
       type: String,
@@ -20,34 +44,43 @@ const customerSchema = new mongoose.Schema(
       validate: [isEmail, "Please Enter a valid Email"],
     },
     address: {
-      type: String,
-      required: [true, "Please Enter Address"],
+      street: {
+        type: String,
+        required: [true, "Please Enter Address"],
+      },
+      city: {
+        type: String,
+        required: [true, "Please Enter City"],
+      },
+      state: {
+        type: String,
+        required: [true, "Please Enter State"],
+      },
+      zip: {
+        type: String,
+        required: [true, "Please Enter Zip Code"],
+        match: /^\d{5}$/,
+      },
     },
-    city: {
-      type: String,
-      required: [true, "Please Enter City"],
-    },
-    state: {
-      type: String,
-      required: [true, "Please Enter State"],
-    },
-    zip: {
-      type: String,
-      required: [true, "Please Enter Zip Code"],
-      match: /^\d{5}$/,
+    hasLivedLessThan6Months: {
+      type: Boolean,
+      required: [
+        true,
+        "Please specify if the customer has lived at the address for less than 6 months",
+      ],
     },
     phoneNumber: {
-      type: String,
-      required: [true, "Please Enter Phone Number"],
+      type: Number,
+      // required: [true, "Please Enter Phone Number"],
       match: /^\d{10}$/,
     },
+
     // password: {
     //   type: String,
     //   required: [true, "Please Enter Password"],
     //   minlength: [8, "Minimum password length is 8"],
     // },
   },
-
   { timestamps: true }
 );
 
